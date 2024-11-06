0d946713c61c1be89fbbadc9aac369fc814680d7
Fernando <fernandobitencourt776@gmail.com>
2024-11-06 09:11:11 -0400

    Created ex07,js

---
+++ aulas/aula11/ex07.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Você foi multado`)
+}
+console.log(`Sempre dirija usando cinto de segurança`)
