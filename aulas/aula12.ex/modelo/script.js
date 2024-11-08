f19913934ff40ff2d72528cd2e86d8ffcedc86b6
Fernando <fernandobitencourt776@gmail.com>
2024-11-07 23:43:40 -0400

    Updated everything

--- aulas/aula12.ex/modelo/script.js
+++ aulas/aula12.ex/modelo/script.js
@@ -1,7 +1,1 @@
-function{
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('foto')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-}
+
