1a4a8e654d8ba8f9f63f63152b3be22fdf9d59ba
Fernando <fernandobitencourt776@gmail.com>
2024-11-07 21:13:58 -0400

    Updated ex11.js script

--- aulas/aula12/ex11.js
+++ aulas/aula12/ex11.js
@@ -1,10 +1,11 @@
-var hora = 2
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora >= 5 && hora < 12){
     console.log(`Bom dia!`)
 } else if (hora >=13 && hora < 18){
     console.log(`Boa tarde!`)
-} else if (hora >= 19 && hora < 0){
+} else if (hora >= 19 && hora < 24){
     console.log(`Boa noite!`)
 } else {
     console.log(`Boa madrugada!`)
