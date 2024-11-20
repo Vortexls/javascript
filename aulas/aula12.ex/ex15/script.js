34b7505ac9c874097c4e379db02f067d7135affe
Fernando <fernandobitencourt776@gmail.com>
2024-11-20 14:01:19 -0400

    Updated JS

--- aulas/aula12.ex/ex15/script.js
+++ aulas/aula12.ex/ex15/script.js
@@ -13,28 +13,30 @@ function verificar(){
         img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             sexo = 'Masculino'
-            if (idade >= 0 || idade <= 12){
-
+            if (idade >= 0 && idade <= 12){
+                img.setAttribute('src', 'imagens/criança-m.png')
             } else if (idade <= 17){
-
+                img.setAttribute('src', 'imagens/jovem-m.png')
             } else if (idade<= 60){
-
+                img.setAttribute('src', 'imagens/adulto-m.png')
             } else if (idade > 60){
-
+                img.setAttribute('src', 'imagens/idoso-m.png')
             }
         } else if (fsex[1].checked){
             sexo = 'Feminino'
-            if (idade >= 0 || idade <= 12){
-
+            if (idade >= 0 && idade <= 12){
+                img.setAttribute('src', 'imagens/criança-f.png')
             } else if (idade <= 17){
-
+                img.setAttribute('src', 'imagens/jovem-f.png')
             } else if (idade<= 60){
-
+                img.setAttribute('src', 'imagens/adulto-f.png')
             } else if (idade > 60){
-
+                img.setAttribute('src', 'imagens/idoso-f.png')
             }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${sexo} com ${idade} anos`
+        res.appendChild(img)
     }
 
 }
