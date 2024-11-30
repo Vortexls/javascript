5b79e96547ebf1921e4fbcaca7cae4bb329063da
Fernando <fernandobitencourt776@gmail.com>
2024-11-30 16:57:49 -0400

    Created ambiente.js

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+
+do{
+    console.log(`Hello World! ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+while (c <= 6){
+    console.log(`Hello World! ${c}`)
+    c ++
+}
+*/
+
+/*
+console.log(`Hello World! ${c}`)
+console.log(`Hello World! ${c}`)
+console.log(`Hello World! ${c}`)
+console.log(`Hello World! ${c}`)
+console.log(`Hello World! ${c}`)
+console.log(`Hello World! ${c}`)
+*/
