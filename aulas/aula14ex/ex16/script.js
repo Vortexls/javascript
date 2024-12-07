a7015b6cfe29ff6b6e21910190867e03667009d0
Fernando <fernandobitencourt776@gmail.com>
2024-12-07 11:41:21 -0400

    Updated index and script

--- aulas/aula14ex/ex16/script.js
+++ aulas/aula14ex/ex16/script.js
@@ -1,1 +1,22 @@
+function calc(){
+    let inicio = document.getElementById('inicio')
+    const fim = document.getElementById('fim')
+    const passo = document.getElementById('passo')
+    const res = document.getElementById('res')
 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('Preencha todo o formulário corretamente para continuar.')
+    } else{
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f ; c += p){
+            res.innerHTML = `${c} ->`
+        }
+    }
+    
+
+
+   
+}
